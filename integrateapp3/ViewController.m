808524a0c673c4e7c401fa974b494db07756e312
dipak2016 <tops1111@Macs-iMac.local>
2016-05-03 11:25:35 +0530

    this is fb integration
    fb integration

--- integrateapp3/ViewController.m
+++ integrateapp3/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
 }
 
 - (void)didReceiveMemoryWarning {
