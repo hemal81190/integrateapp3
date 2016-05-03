4679458f622d00089f01a41e371b35b1929bb763
Mac <tops1111@macs-imac.local>
2016-05-03 10:56:59 +0530

    Initial Commit

---
+++ integrateapp3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  integrateapp3
+//
+//  Created by Mac on 5/3/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ integrateapp3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  integrateapp3
+//
+//  Created by Mac on 5/3/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
